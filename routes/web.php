1044d4d98207da49050c74d0d5945f452a0670d5
Phu <changkhotheky4546@gmail.com>
2018-09-29 22:13:16 +0700

    Checkout with Stripe

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,8 @@ Route::post('/cart/switchToSaveForLater/{product}', 'CartController@switchToSave
 Route::delete('/saveForLater/{product}', 'SaveForLaterController@destroy')->name('saveForLater.destroy');
 Route::post('/saveForLater/switchToCart/{product}', 'SaveForLaterController@switchToCart')->name('saveForLater.switchToCart');
 
-Route::view('/checkout', 'checkout');
-Route::view('/thankyou', 'thankyou');
+Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
+Route::post('/checkout', 'CheckoutController@store')->name('checkout.store');
+
+
+Route::get('/thankyou', 'ConfirmationController@index')->name('confirmation.index');
